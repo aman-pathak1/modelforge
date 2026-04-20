ab261256d230000138941469dad1450d792396c3
aman-12331 <innovativemind38@gmail.com>
2026-04-20 17:32:07 +0530

    add netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      external: ['recharts']
+    }
   }
 })
